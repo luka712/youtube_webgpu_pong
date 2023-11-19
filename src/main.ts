dbafb276ec69f866614a94b6d17fc6cbfcd113ce
Luka Erkapic <erkapic.luka.os@gmail.com>
2023-11-19 08:23:09 +0100

    part4

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { GeometryBuffers } from "./attribute_buffers/GeometryBuffers";
 import { GeometryBuilder } from "./geometry/GeometryBuilder";
+import { Color } from "./math/Color";
+import { Vec2 } from "./math/Vec2";
 import { UnlitRenderPipeline } from "./render_pipelines/UnlitRenderPipeline";
 import { Texture2D } from "./texture/Texture2D";
 
@@ -39,7 +41,7 @@ async function init() {
 
   const image = await loadImage("assets/test_texture.jpeg");
   unlitPipeline.diffuseTexture =  await Texture2D.create(device, image);
-
+  unlitPipeline.textureTilling = new Vec2(5,5);
 
   const draw = () => {
 
